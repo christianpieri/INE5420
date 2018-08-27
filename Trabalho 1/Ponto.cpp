0d10365a418093aea7466b8d09a1bc9cc3502d02
Christian Pieri <chris@bridge.ufsc.br>
2018-08-27 20:34:28 -0300

    inclusão das classes e headers: window, ponto e objeto gráfico

---
+++ Trabalho 1/Ponto.cpp
@@ -0,0 +1,37 @@
+#include "Ponto.hpp"
+
+Ponto::Ponto() {
+}
+
+Ponto::Ponto(const Ponto &origem) {
+}
+
+Ponto::~Ponto() {
+}
+
+Ponto::Ponto(double coordenadaX, double coordenadaY) {
+    x = coordenadaX;
+    y = coordenadaY;
+}
+
+Ponto::Ponto(double coordenadaX, double coordenadaY, std::string _nome) {
+    x = coordenadaX;
+    y = coordenadaY;
+    nome = _nome;
+}
+
+void Ponto::setValorX(double coordenadaX) {
+    x = coordenadaX;
+}
+
+void Ponto::setValorY(double coordenadaY) {
+    y = coordenadaY;
+}                                                                   
+
+double Ponto::getValorX() {
+    return x;
+}
+
+double Ponto::getValorY() {
+    return y;
+}
